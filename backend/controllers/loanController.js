affd2502a78c14b518801f75858ebabd14d48aa6
shivv <shivpratikhande2017@gmail.com>
2024-11-08 17:36:05 +0530

    rm nm

--- backend/controllers/loanController.js
+++ backend/controllers/loanController.js
@@ -1,70 +1,96 @@
 const Loan = require('../models/loan');
-
-let buyDemand = 0;       
-let bidCount = 0;
-let loan = 0;          
-let percentage = 0;
+const moment = require('moment');
 
 function generateUniqueBits() {
     return Math.random() * 100; 
 }
 
-function adjustPriceBasedOnDemandSupply(loan) {
-    const demandEffect = buyDemand * 2;  
-    const adjustedPrice = loan + demandEffect;
+function adjustPriceBasedOnDemandSupply(loan, buyDemand) {
+    const demandEffect = buyDemand * 2; 
+    const adjustedPrice = loan + demandEffect; 
     return adjustedPrice;
 }
 
-exports.setLoan = (req, res) => {
-    const { loan: userLoan, percentage: userPercentage } = req.body;
-    loan = userLoan;  
-    percentage = userPercentage;  
-    res.json({
-        message: `Loan and percentage values have been set.`,
-        loan,
-        percentage
-    });
+exports.setLoan = async (req, res) => {
+    try {
+        const { loan: userLoan, percentage: userPercentage } = req.body;
+        
+        const newLoan = new Loan({
+            loan: userLoan,
+            percentage: userPercentage,
+            bidCount: 0,
+            buyDemand: 0,
+            paidAmount: 0,
+            returnOnLoan: 0,
+            totalLoanValue: userLoan, 
+            bids: [] 
+        });
+
+        await newLoan.save();
+        res.json({
+            message: 'Loan has been created successfully.',
+            loan: newLoan
+        });
+    } catch (err) {
+        res.status(500).json({ message: "Error creating loan", error: err });
+    }
 };
 
 exports.bid = async (req, res) => {
-    const uniqueBits = generateUniqueBits(); 
-    buyDemand += uniqueBits;
-
-    const updatedLoan = adjustPriceBasedOnDemandSupply(loan);
-    const updatedPercentage = percentage - (uniqueBits / 100);
-    const paidAmount = updatedPercentage - percentage;
-    bidCount++;
-
-    percentage = updatedPercentage;
-    loan = updatedLoan;
-
-    const returnOnLoan = loan * percentage / 100;
-    const totalLoanValue = loan + returnOnLoan;
-
-    const loanData = new Loan({
-        loan,
-        percentage: updatedPercentage,
-        bidCount,
-        buyDemand,
-        paidAmount,
-        returnOnLoan,
-        totalLoanValue
-    });
-
     try {
-        const savedLoan = await loanData.save();
+        const { loanId } = req.body;  
+        const uniqueBits = generateUniqueBits();  
+        
+        
+        const bidOpenAt = moment().toDate();  
+        const bidCloseAt = moment().add(30, 'minutes').toDate();  
+        const loan = await Loan.findById(loanId);
+        if (!loan) {
+            return res.status(404).json({ message: 'Loan not found' });
+        }
+
+        const updatedLoan = adjustPriceBasedOnDemandSupply(loan.loan, loan.buyDemand + uniqueBits);
+        
+        const updatedPercentage = loan.percentage - (uniqueBits / 100);
+        
+        const paidAmount = updatedPercentage - loan.percentage;
+        const returnOnLoan = updatedLoan * updatedPercentage / 100;
+        const totalLoanValue = updatedLoan + returnOnLoan;
+
+        const newBid = {
+            uniqueBits,
+            paidAmount,
+            returnOnLoan,
+            bidOpenAt,
+            bidCloseAt,
+            bidAt: moment().toDate(), 
+            status: 'open',  
+        };
+
+        loan.bids.push(newBid);  
+        
+        loan.buyDemand += uniqueBits;  
+        loan.bidCount++;  
+        loan.loan = updatedLoan;  
+        loan.percentage = updatedPercentage;  
+        loan.paidAmount = paidAmount;  
+        loan.returnOnLoan = returnOnLoan;  
+        loan.totalLoanValue = totalLoanValue;
+
+        
+        const savedLoan = await loan.save();
+
         res.json({
-            message: `You have bought assets with unique bits factor of ${uniqueBits.toFixed(2)}.`,
+            message: `Bid placed successfully with unique bits factor of ${uniqueBits.toFixed(2)}.`,
             paidAmount: paidAmount.toFixed(2),
             currentPercentage: updatedPercentage.toFixed(2),
             returnOnLoan: returnOnLoan.toFixed(2),
-            demand: buyDemand.toFixed(2),
-            TotalLoanValue: totalLoanValue.toFixed(2),
-            bidCount: bidCount,
-            loan,
-            savedLoan
+            demand: loan.buyDemand.toFixed(2),
+            totalLoanValue: totalLoanValue.toFixed(2),
+            bidCount: loan.bidCount,
+            loan: savedLoan
         });
     } catch (err) {
-        res.status(500).json({ message: "Error saving loan data", error: err });
+        res.status(500).json({ message: "Error placing bid", error: err });
     }
 };
--- backend/routes/loanRoutes.js
+++ backend/routes/loanRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const router = express.Router();  
+const router = express.Router();
 const loanController = require('../controllers/loanController');
 
-router.post('/loan', loanController.setLoan);  
-router.post('/bid', loanController.bid);      
+router.post('/loan', loanController.setLoan); 
+
+router.post('/bid', loanController.bid);
 
 module.exports = router;
