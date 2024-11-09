eaab932599ebddc7be9097d1e2b3b906cff78bc1
Parth Takale <130144456+Parthtakale06@users.noreply.github.com>
2024-11-09 18:37:53 +0530

    added changes

--- backend/controllers/loanController.js
+++ backend/controllers/loanController.js
@@ -1,7 +1,5 @@
 const Loan = require("../models/loan");
 const moment = require("moment");
-const Loan = require("../models/loan");
-const moment = require("moment");
 
 function generateUniqueBits() {
   return Math.random() * 100;
