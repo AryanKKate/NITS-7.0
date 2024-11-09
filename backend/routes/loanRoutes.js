045fd6edfc571b1eb42fe36e115f78b1293a8268
Parth Takale <130144456+Parthtakale06@users.noreply.github.com>
2024-11-09 15:13:49 +0530

    added changes

--- backend/routes/loanRoutes.js
+++ backend/routes/loanRoutes.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const loanController = require('../controllers/loanController');
+const loanController = require("../controllers/loanController");
 
-router.get('/', loanController.getLoan)
-router.post('/', loanController.setLoan); 
-router.post('/bid', loanController.bid);
+router.get("/", loanController.getLoan);
+router.post("/", loanController.setLoan);
+router.post("/bid", loanController.bid);
 
-router.post("/bid_counts",loanController.bid_count )
+router.post("/bid_counts", loanController.bid_count);
+router.post("/getAllPercentage", loanController.all_percentage);
 
 module.exports = router;
